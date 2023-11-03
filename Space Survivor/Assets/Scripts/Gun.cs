a262ea5fa85478f0ded7118433b8daaea60461ba
Samuel <k>
2023-11-02 21:50:02 -0600

    Added Player Character files

---
+++ Space Survivor/Assets/Scripts/Gun.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firepoint;
+    private float fireForce = 20f;
+
+    void Update()
+    {
+        Shoot();
+    }
+
+    public void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+        bullet.GetComponent<Rigidbody2D>().AddForce(firepoint.up * fireForce, ForceMode2D.Impulse);
+    }
+
+}
