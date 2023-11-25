15f662781d1eefec95fecb9dc1b36d242ff5ff35
kaurv <van491@usask.ca>
2023-11-25 15:42:26 -0600

    Created a new buzz boss enemy. The player is successfully being shot at and its health is reducing with every strike.

---
+++ Space Survivor/Assets/Scripts/BuzzBulletScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuzzBulletScript : MonoBehaviour
+{
+    private GameObject player;
+    private Rigidbody2D rb;
+    public float force = 5;
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        Vector3 direction = player.transform.position - transform.position;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot+90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > 10){
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.gameObject.CompareTag("Player")){
+            other.gameObject.GetComponent<PlayerController>().currentHealth -= 20;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Space Survivor/Assets/Scripts/BuzzEnemy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuzzEnemy : MonoBehaviour
+{
+    private GameObject player;
+    public GameObject bullet;
+    public Transform bulletPos;
+    private float timer; //to control the frequncy that the bullet spawn into the scene
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+        Debug.Log(distance);
+
+        if (distance < 10){
+            timer += Time.deltaTime;
+
+            if (timer > 2){
+            timer = 0;
+            shoot();
+        }
+        }
+
+        
+    }
+
+    void shoot(){
+        Instantiate(bullet, bulletPos.position, Quaternion.identity);
+    }
+}
